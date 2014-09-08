7ddf7d6076466a5ae9dbf75662faab886e64b5a0
Maxime Arnstamm <maxime.arnstamm@gmail.com>
2014-09-08 23:39:46 +0200

    exo 4

--- Advanced/01-Ruby/01-Programming-basics/04-Stupid-coaching/lib/coach_answer.rb
+++ Advanced/01-Ruby/01-Programming-basics/04-Stupid-coaching/lib/coach_answer.rb
@@ -1,8 +1,20 @@
 def coach_answer(your_message)
   # TODO: return coach answer to your_message
+  if your_message == "I am going to work right now SIR !"
+    ""
+  elsif your_message.end_with? "?"
+      "Silly question, get dressed and go to work !"
+  else
+      "I don't care son, get dressed and go to work !"
+  end
 end
 
 def coach_answer_enhanced(your_message)
   # TODO: return coach answer to your_message, with additional custom rules of yours !
+  if your_message.empty?
+    "Speak up or get to work"
+  else
+    coach_answer your_message
+  end
 end
 
--- Advanced/01-Ruby/01-Programming-basics/04-Stupid-coaching/lib/interface.rb
+++ Advanced/01-Ruby/01-Programming-basics/04-Stupid-coaching/lib/interface.rb
@@ -1,3 +1,9 @@
 require_relative "coach_answer"
 #TODO: Implement the program that makes you discuss with your coach from the terminal.
+answer = "init"
 
+while !answer.empty?
+  question = gets.strip
+  answer = coach_answer question
+  puts answer
+end
