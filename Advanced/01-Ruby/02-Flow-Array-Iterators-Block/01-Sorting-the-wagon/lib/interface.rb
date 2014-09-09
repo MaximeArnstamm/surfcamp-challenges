654801987c4b4a6ba7b39922df2363e8939442d1
Maxime Arnstamm <maxime.arnstamm@gmail.com>
2014-09-09 16:52:32 +0200

    exo1.2.1 ok

--- Advanced/01-Ruby/02-Flow-Array-Iterators-Block/01-Sorting-the-wagon/lib/interface.rb
+++ Advanced/01-Ruby/02-Flow-Array-Iterators-Block/01-Sorting-the-wagon/lib/interface.rb
@@ -8,7 +8,20 @@ begin
   name.chomp! if name
 
   # TODO: Add the name we just gotto the students array
+  students << name unless name.empty?
 
 end while name != ""
 
 # TODO: Call `wagon_sort` method and display the sorted student list
+students = wagon_sort students
+
+if (students.empty?)
+  combine = ""
+elsif (students.size == 1)
+  combine = students.first
+else
+  combine = students.slice(0...-1).join(", ") + " and " + students.last
+end
+
+puts "Congratulations! Your Wagon has #{students.size} students:
+- #{combine}"
