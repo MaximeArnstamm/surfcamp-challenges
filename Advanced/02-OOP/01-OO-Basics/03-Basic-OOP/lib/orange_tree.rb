d188241f9cf8650af877bf9d70ae2a3a81f7e418
Maxime Arnstamm <maxime.arnstamm@gmail.com>
2014-09-10 17:21:33 +0200

    exo 2.1.3 ok

--- Advanced/02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
+++ Advanced/02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
@@ -1,3 +1,38 @@
 class OrangeTree
-  # TODO: Implement all the specs defined in the README.md :)
+  attr_reader :height, :fruits
+
+  def initialize
+    @age = 0
+    @height = 0
+    @dead = false
+    @fruits = 0
+  end
+
+  def one_year_passes!
+    unless @dead
+      @age += 1
+
+      if (@height < 10)
+        @height += 1
+      end
+
+      @dead = rand(50) > 100 - @age
+
+      @fruits = 0
+      if (@age > 5 && @age <= 10)
+        @fruits = 100
+      end
+      if (@age > 10 && @age <= 15)
+        @fruits = 200
+      end
+    end
+  end
+
+  def pick_a_fruit!
+    @fruits -= 1
+  end
+
+  def dead?
+    @dead
+  end
 end
