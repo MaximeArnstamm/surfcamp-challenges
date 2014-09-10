84f1cae2dbc6893163d88a28eb65a6a4c24abb19
Maxime Arnstamm <maxime.arnstamm@gmail.com>
2014-09-10 10:43:14 +0200

    exo opt1 ok

--- Advanced/01-Ruby/03-Hash-Symbols/Optional-01-Roman-to-integer/lib/roman_to_integer.rb
+++ Advanced/01-Ruby/03-Hash-Symbols/Optional-01-Roman-to-integer/lib/roman_to_integer.rb
@@ -1,40 +1,32 @@
 def roman_to_integer(roman_string)
   #TODO: translate roman string to integer
-  roman_numbers ={
-    'I' => 1,
-    'V' => 5,
-    'X' => 10,
-    'L' => 50,
-    'C' => 100,
-    'D' => 500,
-    'M' => 1000
+  roman_numbers = {
+    "I" => 1,
+    "V" => 5,
+    "X" => 10,
+    "L" => 50,
+    "C" => 100,
+    "D" => 500,
+    "M" => 1000,
+    "IV" => 4,
+    "IX" => 9,
+    "XC" => 90,
+    "CM" => 900
   }
 
-  my_numbers_array = []
+  roman_numbers_order = ["CM", "M", "D", "XC", "C", "L", "IX", "X", "IV", "V", "I"]
 
-  roman_string.split('').each do |nombre|
-    roman_numbers.each do |key,value|
-      if nombre == key
-        my_numbers_array << value
-      end
-    end
+  roman_numbers_order.reduce(0) do |sum, r|
+      nb = roman_string.scan(r).size
+      roman_string.slice!(r)
+      sum + nb * roman_numbers[r]
   end
-  if my_numbers_array.size == 2
-    if my_numbers_array[0] < my_numbers_array[1]
-      my_numbers_array[0] = - my_numbers_array[0]
-    end
-  else
-    for i in 1..my_numbers_array.size-2
-      if my_numbers_array[i] < my_numbers_array[i+1] && my_numbers_array[i] != my_numbers_array[i-1]
-        my_numbers_array[i] = - my_numbers_array[i]
-      end
-    end
-  end
-  my_numbers_array.inject{|sum,x| sum + x }
 end
 
 # testing your program
-puts roman_to_integer('IV') == 4 # => true
-puts roman_to_integer('XII') == 12 # => true
-puts roman_to_integer('XIX') == 19 # => true
-puts roman_to_integer('XVIII')
+puts roman_to_integer("II")
+puts roman_to_integer("IV")
+puts roman_to_integer("IV") == 4 # => true
+puts roman_to_integer("XII") == 12 # => true
+puts roman_to_integer("XIX") == 19 # => true
+puts roman_to_integer("XVIII")
