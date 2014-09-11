9be9a9542299462ab996bdd6037e1653a22ca5bc
Maxime Arnstamm <maxime.arnstamm@gmail.com>
2014-09-11 10:18:31 +0200

    exo 2.2.2 ok

--- Advanced/02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
+++ Advanced/02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
@@ -1,11 +1,27 @@
 class Restaurant
-  
+
   #TODO add relevant accessors if necessary
-  
+  attr_accessor :city, :name
+
   def initialize(city, name)
     #TODO: implement constructor with relevant instance variables
+    @city = city
+    @name = name
+    @total_rate = 0
+    @nb_rates = 0
   end
-  
+
+  def rate(rate)
+    @nb_rates += 1
+    @total_rate += rate
+  end
+
+  def average_rating
+    @total_rate / @nb_rates.to_f
+  end
+
   #TODO: implement #filter_by_city and #rate methods
-  
+  def self.filter_by_city(list, city)
+    list.select { |resto| resto.name if (resto.city == city) }
+  end
 end
